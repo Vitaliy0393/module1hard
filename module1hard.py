efe25c25479959bfb832405f4d6787d5c2637057
Vitaliy0393 <167925531+Vitaliy0393@users.noreply.github.com>
2024-05-09 23:45:35 +0300

    Add files via upload

--- module1hard.py
+++ module1hard.py
@@ -1,19 +1,19 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = sorted(students)
+#print(students)
 
 sum_grades = sum(grades[0]),sum(grades[1]),sum(grades[2]),sum(grades[3]),sum(grades[4])
 #print(sum_grades)
-midlle_ball = sum_grades[0]/5, sum_grades[1]/4, sum_grades[2]/4, sum_grades[3]/3, sum_grades[4]/5
+
+grades_index = len(grades[0]), len(grades[1]), len(grades[2]), len(grades[3]), len(grades[4])
+#print(grades_index)
+
+midlle_ball = sum_grades[0]/len(grades[0]), sum_grades[1]/len(grades[1]), sum_grades[2]/len(grades[2]), sum_grades[3]/len(grades[3]), sum_grades[4]/len(grades[4])
 #print(midlle_ball)
 
 
-#students = {'Johnny':[4, 5, 5, 2], 'Bilbo':[2, 2, 2, 3], 'Steve':[5, 5, 5, 4, 5], 'Khendrik':[4, 4, 3],'Aaron':[5, 3, 3, 5, 4]}
-students = list
-#print(students,type(students))
-students = [['Johnny'], ['Bilbo'], ['Steve'], ['Khendrik'], ['Aaron']]
-#print(students)
-
-midlle_ball_students = {'Johnny':midlle_ball[0], 'Bilbo':midlle_ball[1], 'Steve':midlle_ball[4], 'Khendrik':midlle_ball[3],'Aaron':midlle_ball[2]}
+midlle_ball_students = { students[0]:midlle_ball[0], students[1]:midlle_ball[1], students[2]:midlle_ball[2], students[3]:midlle_ball[3],students[4]:midlle_ball[4]}
 print(midlle_ball_students)
 
 
